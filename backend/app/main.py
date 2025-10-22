da5d076123e97a8cfcbd9bc36d1ce3f78d92f13f
Valadis-Mastoras <valadisprf94@gmail.com>
2025-10-22 16:31:18 +0300

    Minor updates

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,14 @@
 import os
 from dotenv import load_dotenv
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from llm import get_response
 
+# Load environment variables from .env
+load_dotenv()
 
-load_dotenv()  # loads .env
 groq_api_key = os.getenv("GROQ_API_KEY")
-postgress_db_url = os.getenv("POSTGRESS_URL")
+postgres_db_url = os.getenv("POSTGRES_URL")
 
 app = Flask(__name__)
 CORS(app)
@@ -19,7 +19,6 @@ def chat():
     user_message = data.get("message")
 
     llm_response = get_response(user_message, groq_api_key)
-
     return jsonify({"response": llm_response})
 
 if __name__ == "__main__":
