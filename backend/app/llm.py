96e0e507c16452f306d73fa7e0762e8d0bbdf417
Valadis-Mastoras <valadisprf94@gmail.com>
2025-11-29 11:57:14 +0200

    Added a basic memory component

--- backend/app/llm.py
+++ backend/app/llm.py
@@ -1,29 +1,38 @@
-from groq import Groq
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.messages import HumanMessage
+
+from memory import get_session_history
+
 
 # Function to translate text into a target language
 def get_response(message: str, api_key) -> str:
-
-    client = Groq(api_key=api_key, max_retries=1)
 
     template = f"""
         You are a normal chatbot which, only when asked, can answer questions about Philosophy. 
         
         Give a direct final response which should be a normal text, for a human to read it.
-
-        Respond to following user input.
-
-        Text:
-        \"\"\"{message}\"\"\"
     """
 
-    response = client.chat.completions.create(
-        model="openai/gpt-oss-20b",
-        messages=[
-            {"role": "system", "content": template},
-            {"role": "user", "content": message}
-        ],
-        temperature=0,
-        max_tokens=2000
-    )
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", template),
+        ("user", "{input}")
+    ])
 
-    return response.choices[0].message.content.strip()
+    llm = ChatGroq(model="openai/gpt-oss-20b", temperature=0.7, groq_api_key=api_key)
+
+    chain = prompt | llm
+
+    with_message_history = RunnableWithMessageHistory(
+            chain, 
+            get_session_history,
+            input_messages_key="input"
+        )
+    
+    
+    config={"configurable":{"session_id":"chat1"}}
+    
+    response = with_message_history.invoke({"input": [HumanMessage(content=message)], "language":"Greek"}, config=config)
+
+    return response.content
